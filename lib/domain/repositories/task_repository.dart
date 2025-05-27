3372f2c60daab8d892ed22a416cad0174abcfb38
Jordan Kamga Wafo <jordankamga17@gmail.com>
2025-05-27 09:26:29 +0100

    Repositories

--- lib/domain/repositories/task_repository.dart
+++ lib/domain/repositories/task_repository.dart
@@ -1,8 +1,10 @@
 import 'package:clean_task_app/domain/entities/task.dart';
+import 'package:clean_task_app/domain/entities/task_priority.dart';
 
 abstract class TaskRepository {
   Future<int> createTask(Task task);
   Future<List<Task>> getTasks();
   Future<void> updateTask(Task task);
+  Future<void> updateTaskPriority(int taskId, TaskPriority newPriority); // Nouvelle méthode
   Future<void> deleteTask(int id);
 }
